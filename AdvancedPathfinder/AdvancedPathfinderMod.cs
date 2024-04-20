1dcab6a30ed71c12a7049c74f3e0def7d6aa11ce
grovre <landonvee111@gmail.com>
2024-04-20 18:24:53 -0500

    Delete commented code and invert if

--- AdvancedPathfinder/AdvancedPathfinderMod.cs
+++ AdvancedPathfinder/AdvancedPathfinderMod.cs
@@ -79,39 +79,22 @@ namespace AdvancedPathfinder
             }
         }
         
-        
-/*        [HarmonyPostfix]
-        [HarmonyPatch(typeof(VehiclePathfinder<TrackConnection, TrackPathNode, Train>), "FindImmediately")]
-        private static void TrainPathfinder_FindImmediately_pof(VehiclePathfinder<TrackConnection, TrackPathNode, Train> __instance)
-        {
-            FileLog.Log("FindImmediately");
-        }*/
-
-//        private static double _origMs = 0;
-//        private static double _newMs = 0;
         [HarmonyPrefix]
         [HarmonyPatch(typeof(Train), "TryFindPath")]
         [HarmonyPriority(Priority.VeryLow)]
         private static bool Train_TryFindPath_prf(Train __instance, ref bool __result, TrackConnection origin, IVehicleDestination target, List<TrackConnection> result)
         {
             RailPathfinderManager manager = Manager<RailPathfinderManager>.Current;
-            if (manager != null)
-            {
-                //List<TrackConnection> resultList = new();
-                result.Clear();
-                bool result2 = manager.FindImmediately(__instance, (RailConnection) origin, target, result);
-                ShowUpdatePathHint(manager.ElapsedMilliseconds, __instance);
-//                manager.Stats?.AddPathfindingTime(manager.ElapsedMilliseconds);
-//                _origMs += TrainPathfinder.Current.ElapsedMilliseconds;
-//                _newMs += manager.ElapsedMilliseconds;
-//                float blockUpdatesMs = SimpleLazyManager<RailBlockHelper>.Current.ElapsedMilliseconds;
- //               FileLog.Log("Finding path, result={0}, in {1}ms (original was in {2}ms)".Format(result2.ToString(), manager.ElapsedMilliseconds.ToString("N2"), TrainPathfinder.Current.ElapsedMilliseconds.ToString("N2")));
-//                FileLog.Log(string.Format("Total original = {0:N0}ms, new = {1:N0}ms, ratio = {2:N1}%, block updates = {3:N2}ms", (_origMs), _newMs+blockUpdatesMs, (_origMs / (_newMs+blockUpdatesMs) * 100f), blockUpdatesMs));
-                __result = result2;
-                return false;
-            }
-
-            return true;
+            if (manager == null)
+                return true;
+            
+            result.Clear();
+            bool result2 = manager.FindImmediately(__instance, (RailConnection) origin, target, result);
+            ShowUpdatePathHint(manager.ElapsedMilliseconds, __instance);
+            __result = result2;
+            
+            // Don't continue with original pathfinding method
+            return false;
         }
 
         protected override void OnLateUpdate()
